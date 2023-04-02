7c772dc67c69e89061eff33a75577ee11498dded
elfallah <elfallahmohamed.1@gmail.com>
2023-04-02 22:44:37 +0000

    refactoring

--- o2a/o2a_libs/el_coord_functions.py
+++ o2a/o2a_libs/el_coord_functions.py
@@ -48,6 +48,50 @@ def end_of_months(n: str) -> str:  # pylint:disable=invalid-name
     return f"59 23 L */{n} *"
 
 
+def calculate_current_n(
+    initial_instance: datetime, frequency: int, execution_time: datetime, n: int
+) -> datetime:
+    """
+    Calculate the datetime of the dataset produced shifted by n
+    calculated using the following formula :
+    initial_instance : dataset initial-instance (datetime)
+    frequency : dataset frequency (minutes)
+    execution_time: coordinator action creation (materialization) nominal time <=> Dag execution time
+    return initial_instance + frequency * ((execution_time - initial_instance) div frequency + n)
+    """
+    current_n = initial_instance + timedelta(
+        minutes=frequency * (((execution_time - initial_instance).total_seconds() // 60) // frequency + n)
+    )
+    return current_n
+
+
+def resolve_dataset_template(template: str, datetime_data: datetime) -> str:
+    """
+    Given a template, resolve the variables MINUTE, HOUR, MONTH, DAY, YEAR using the datetime data provided
+    :param template: string with variables MINUTE... inside ${...} e.g : hdfs://test/${YEAR}/${MONTH}
+    :param datetime_data: datetime information e.g : 2023-05-01T05:00
+    :return: string representing resolved template e.g : hdfs://test/2023/05
+    """
+    template = template.replace("{", "")
+    template = template.replace("}", "")
+    resolve_template_map = {
+        r"\$MINUTE": str(datetime_data.minute)
+        if len(str(datetime_data.minute)) > 1
+        else "0" + str(datetime_data.minute),
+        r"\$HOUR": str(datetime_data.hour)
+        if len(str(datetime_data.hour)) > 1
+        else "0" + str(datetime_data.hour),
+        r"\$DAY": str(datetime_data.day) if len(str(datetime_data.day)) > 1 else "0" + str(datetime_data.day),
+        r"\$MONTH": str(datetime_data.month)
+        if len(str(datetime_data.month)) > 1
+        else "0" + str(datetime_data.month),
+        r"\$YEAR": str(datetime_data.year),
+    }
+    for word, replacement in resolve_template_map.items():
+        template = re.sub(word, replacement, template)
+    return template
+
+
 @contextfunction
 def current(context=None, n: int = None):  # pylint:disable=invalid-name
     """
@@ -67,29 +111,13 @@ def current(context=None, n: int = None):  # pylint:disable=invalid-name
         dataset = find_dataset_by_name(datasets, dataset_name)
 
         if dataset:
-            exec_time = context.get("ts")[:16]
-            dag_run = datetime.strptime(exec_time, "%Y-%m-%dT%H:%M")
+            execution_time = context.get("ts")[:16]
+            execution_time = datetime.strptime(execution_time, "%Y-%m-%dT%H:%M")
             initial_instance = datetime.strptime(dataset.initial_instance[:-1], "%Y-%m-%dT%H:%M")
-            template = dataset.uri_template
-            template = template.replace("{", "")
-            template = template.replace("}", "")
             frequency = int(dataset.frequency)
-            current_n = initial_instance + timedelta(
-                minutes=frequency * (((dag_run - initial_instance).total_seconds() // 60) // frequency + n)
-            )
-            resolve_template_map = {
-                r"\$MINUTE": str(current_n.minute)
-                if len(str(current_n.minute)) > 1
-                else "0" + str(current_n.minute),
-                r"\$HOUR": str(current_n.hour) if len(str(current_n.hour)) > 1 else "0" + str(current_n.hour),
-                r"\$DAY": str(current_n.day) if len(str(current_n.day)) > 1 else "0" + str(current_n.day),
-                r"\$MONTH": str(current_n.month)
-                if len(str(current_n.month)) > 1
-                else "0" + str(current_n.month),
-                r"\$YEAR": str(current_n.year),
-            }
-            for word, replacement in resolve_template_map.items():
-                template = re.sub(word, replacement, template)
-            return template
+            template = dataset.uri_template
+            current_n = calculate_current_n(initial_instance, frequency, execution_time, n)
+            dataset_uri = resolve_dataset_template(template, current_n)
+            return dataset_uri
 
     return None
